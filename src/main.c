2fe7bd643d03e8ac753eb039f8842ed4a444d574
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2024-11-01 03:41:34 +0800

    main.c: Add support for linked list test case
    ;)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <include/tui.h>
-
+#include <include/linked_list.h>
 int main(){
     startGameUi();
     return 0;
+}
+
+void testLinkedList(){
+    Node* head;
+
+    // create a ridiculous amount of nodes
+    head = createNode(1, 1, 1, NULL, NULL);
+    for(int j = 0; j < 10000; j++){
+        for(int i = 0; i<10; i++){
+            Node* curMove = createNode(1, i, i, NULL, NULL);
+            insertNodeHead(&head, curMove);
+        }
+    }
+    printList(head);
+    destroyList(head); // test the free() function
+    return;
 }
